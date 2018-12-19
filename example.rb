cfdc0b227d0a3ec0e0c18b9e088ec3b7e641c109
Zoë Sparks <zoe@milky.flowers>
2018-12-19 02:31:57 -0500

    Make draw.rb function as an example and rename
    Add other possible arguments to TotalisticCA.new commented-out to
    facilitate experimentation. Rename to example.rb.

--- example.rb
+++ example.rb
@@ -1,7 +1,7 @@
 require_relative 'totalistic_ca'
 require_relative 'rule_drawer'
 
-rule_1037 = TotalisticCA.new(
+ca = TotalisticCA.new(
   rule: [1, 0, 2, 0, 1, 0, 1],
   rows: 512,
   first_row: [
@@ -21,36 +21,28 @@ rule_1037 = TotalisticCA.new(
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0,
   ],
-  borders: :toroidal
+#  borders: :unconnected,
+#  unconnected_value: 2,
+#  columns: 100,
 )
 
 colors = [
   {
-    red: 0.88,
-    green: 0.26,
-    blue: 0.71
+    red: 224,
+    green: 66,
+    blue: 181,
   },
   {
-    red: 0.76,
-    green: 0.92,
-    blue: 0.99
+    red: 194,
+    green: 235,
+    blue: 252,
   },
   {
-    red: 1,
-    green: 0.98,
-    blue: 0.97
+    red: 255,
+    green: 250,
+    blue: 247,
   },
 ]
 
-#results = rule_1037.run.flatten
-#zero = results.count(0)
-#one = results.count(1)
-#two = results.count(2)
-#total = zero + one + two
-#
-#puts "0: #{zero} (#{zero.to_f / total})"
-#puts "1: #{one} (#{one.to_f / total})"
-#puts "2: #{two} (#{two.to_f / total})"
-
-drawer = RuleDrawer.new(rule_1037.run(scale: 6), colors)
-drawer.build_image.save("1037_6_29.png")
+drawer = RuleDrawer.new(ca.run(scale: 3), colors)
+drawer.build_image.save("example.png")
